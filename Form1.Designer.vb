063d6aabcb017d7ed720321f55191246d318c69c
HumXC <Hum-XC@outlook.com>
2021-08-09 14:35:53 +0800

    增加了鼠标交互，优化UI显示

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Title = New System.Windows.Forms.Label()
         Me.Code_text = New System.Windows.Forms.Label()
@@ -33,21 +34,31 @@ Partial Class Form1
         Me.Music_Button = New System.Windows.Forms.RadioButton()
         Me.Code_Button = New System.Windows.Forms.RadioButton()
         Me.Code_ico = New System.Windows.Forms.PictureBox()
-        Me.Music_ioc = New System.Windows.Forms.PictureBox()
+        Me.Music_ico = New System.Windows.Forms.PictureBox()
         Me.Game_ico = New System.Windows.Forms.PictureBox()
         Me.Default_ico = New System.Windows.Forms.PictureBox()
         Me.Default_line = New System.Windows.Forms.PictureBox()
         Me.Game_line = New System.Windows.Forms.PictureBox()
         Me.Music_line = New System.Windows.Forms.PictureBox()
         Me.Code_line = New System.Windows.Forms.PictureBox()
+        Me.Animation_up = New System.Windows.Forms.Timer(Me.components)
+        Me.Animation_down = New System.Windows.Forms.Timer(Me.components)
+        Me.Code_line_back = New System.Windows.Forms.PictureBox()
+        Me.Music_line_back = New System.Windows.Forms.PictureBox()
+        Me.Game_line_back = New System.Windows.Forms.PictureBox()
+        Me.Default_line_back = New System.Windows.Forms.PictureBox()
         CType(Me.Code_ico, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Music_ioc, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Music_ico, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Game_ico, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Default_ico, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Default_line, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Game_line, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Music_line, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Code_line, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Code_line_back, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Music_line_back, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Game_line_back, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Default_line_back, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Title
@@ -70,7 +81,7 @@ Partial Class Form1
         Me.Code_text.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Code_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold)
         Me.Code_text.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Code_text.Location = New System.Drawing.Point(465, 15)
+        Me.Code_text.Location = New System.Drawing.Point(506, 30)
         Me.Code_text.Name = "Code_text"
         Me.Code_text.Size = New System.Drawing.Size(119, 160)
         Me.Code_text.TabIndex = 9
@@ -83,7 +94,7 @@ Partial Class Form1
         Me.Music_text.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Music_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold)
         Me.Music_text.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Music_text.Location = New System.Drawing.Point(328, 15)
+        Me.Music_text.Location = New System.Drawing.Point(359, 30)
         Me.Music_text.Name = "Music_text"
         Me.Music_text.Size = New System.Drawing.Size(118, 160)
         Me.Music_text.TabIndex = 11
@@ -96,7 +107,7 @@ Partial Class Form1
         Me.Game_text.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Game_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold)
         Me.Game_text.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Game_text.Location = New System.Drawing.Point(193, 15)
+        Me.Game_text.Location = New System.Drawing.Point(214, 30)
         Me.Game_text.Name = "Game_text"
         Me.Game_text.Size = New System.Drawing.Size(128, 160)
         Me.Game_text.TabIndex = 12
@@ -109,7 +120,7 @@ Partial Class Form1
         Me.Defau_text.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Defau_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold)
         Me.Defau_text.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Defau_text.Location = New System.Drawing.Point(52, 15)
+        Me.Defau_text.Location = New System.Drawing.Point(62, 30)
         Me.Defau_text.Name = "Defau_text"
         Me.Defau_text.Size = New System.Drawing.Size(129, 160)
         Me.Defau_text.TabIndex = 13
@@ -167,54 +178,58 @@ Partial Class Form1
         '
         Me.Code_ico.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Code_ico.Image = Global.DesktopSpace.My.Resources.Resources.code
-        Me.Code_ico.Location = New System.Drawing.Point(462, 25)
+        Me.Code_ico.Location = New System.Drawing.Point(503, 40)
         Me.Code_ico.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Code_ico.Name = "Code_ico"
         Me.Code_ico.Size = New System.Drawing.Size(124, 115)
         Me.Code_ico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.Code_ico.TabIndex = 21
         Me.Code_ico.TabStop = False
+        Me.Code_ico.Tag = "Code"
         '
-        'Music_ioc
+        'Music_ico
         '
-        Me.Music_ioc.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Music_ioc.Image = Global.DesktopSpace.My.Resources.Resources.音乐_音符
-        Me.Music_ioc.Location = New System.Drawing.Point(332, 39)
-        Me.Music_ioc.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.Music_ioc.Name = "Music_ioc"
-        Me.Music_ioc.Size = New System.Drawing.Size(112, 93)
-        Me.Music_ioc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.Music_ioc.TabIndex = 20
-        Me.Music_ioc.TabStop = False
+        Me.Music_ico.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Music_ico.Image = Global.DesktopSpace.My.Resources.Resources.音乐_音符
+        Me.Music_ico.Location = New System.Drawing.Point(363, 54)
+        Me.Music_ico.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Music_ico.Name = "Music_ico"
+        Me.Music_ico.Size = New System.Drawing.Size(112, 93)
+        Me.Music_ico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Music_ico.TabIndex = 20
+        Me.Music_ico.TabStop = False
+        Me.Music_ico.Tag = "Music"
         '
         'Game_ico
         '
         Me.Game_ico.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Game_ico.Image = Global.DesktopSpace.My.Resources.Resources.手柄2
-        Me.Game_ico.Location = New System.Drawing.Point(197, 39)
+        Me.Game_ico.Location = New System.Drawing.Point(218, 54)
         Me.Game_ico.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Game_ico.Name = "Game_ico"
         Me.Game_ico.Size = New System.Drawing.Size(118, 93)
         Me.Game_ico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.Game_ico.TabIndex = 19
         Me.Game_ico.TabStop = False
+        Me.Game_ico.Tag = "Game"
         '
         'Default_ico
         '
         Me.Default_ico.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Default_ico.Image = Global.DesktopSpace.My.Resources.Resources.云桌面
-        Me.Default_ico.Location = New System.Drawing.Point(45, 31)
+        Me.Default_ico.Location = New System.Drawing.Point(55, 46)
         Me.Default_ico.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Default_ico.Name = "Default_ico"
         Me.Default_ico.Size = New System.Drawing.Size(138, 117)
         Me.Default_ico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.Default_ico.TabIndex = 18
         Me.Default_ico.TabStop = False
+        Me.Default_ico.Tag = "Default"
         '
         'Default_line
         '
         Me.Default_line.BackColor = System.Drawing.Color.Lime
-        Me.Default_line.Location = New System.Drawing.Point(45, 181)
+        Me.Default_line.Location = New System.Drawing.Point(55, 196)
         Me.Default_line.Name = "Default_line"
         Me.Default_line.Size = New System.Drawing.Size(138, 9)
         Me.Default_line.TabIndex = 24
@@ -223,7 +238,7 @@ Partial Class Form1
         'Game_line
         '
         Me.Game_line.BackColor = System.Drawing.Color.Lime
-        Me.Game_line.Location = New System.Drawing.Point(204, 181)
+        Me.Game_line.Location = New System.Drawing.Point(225, 196)
         Me.Game_line.Name = "Game_line"
         Me.Game_line.Size = New System.Drawing.Size(104, 9)
         Me.Game_line.TabIndex = 25
@@ -232,7 +247,7 @@ Partial Class Form1
         'Music_line
         '
         Me.Music_line.BackColor = System.Drawing.Color.Lime
-        Me.Music_line.Location = New System.Drawing.Point(338, 181)
+        Me.Music_line.Location = New System.Drawing.Point(369, 196)
         Me.Music_line.Name = "Music_line"
         Me.Music_line.Size = New System.Drawing.Size(104, 9)
         Me.Music_line.TabIndex = 26
@@ -241,11 +256,59 @@ Partial Class Form1
         'Code_line
         '
         Me.Code_line.BackColor = System.Drawing.Color.Lime
-        Me.Code_line.Location = New System.Drawing.Point(470, 181)
+        Me.Code_line.Location = New System.Drawing.Point(511, 196)
         Me.Code_line.Name = "Code_line"
         Me.Code_line.Size = New System.Drawing.Size(104, 9)
         Me.Code_line.TabIndex = 27
         Me.Code_line.TabStop = False
+        '
+        'Animation_up
+        '
+        Me.Animation_up.Interval = 10
+        '
+        'Animation_down
+        '
+        Me.Animation_down.Interval = 10
+        '
+        'Code_line_back
+        '
+        Me.Code_line_back.BackColor = System.Drawing.Color.Gold
+        Me.Code_line_back.Location = New System.Drawing.Point(511, 224)
+        Me.Code_line_back.Name = "Code_line_back"
+        Me.Code_line_back.Size = New System.Drawing.Size(104, 9)
+        Me.Code_line_back.TabIndex = 31
+        Me.Code_line_back.TabStop = False
+        Me.Code_line_back.Visible = False
+        '
+        'Music_line_back
+        '
+        Me.Music_line_back.BackColor = System.Drawing.Color.Gold
+        Me.Music_line_back.Location = New System.Drawing.Point(369, 224)
+        Me.Music_line_back.Name = "Music_line_back"
+        Me.Music_line_back.Size = New System.Drawing.Size(104, 9)
+        Me.Music_line_back.TabIndex = 30
+        Me.Music_line_back.TabStop = False
+        Me.Music_line_back.Visible = False
+        '
+        'Game_line_back
+        '
+        Me.Game_line_back.BackColor = System.Drawing.Color.Gold
+        Me.Game_line_back.Location = New System.Drawing.Point(225, 224)
+        Me.Game_line_back.Name = "Game_line_back"
+        Me.Game_line_back.Size = New System.Drawing.Size(104, 9)
+        Me.Game_line_back.TabIndex = 29
+        Me.Game_line_back.TabStop = False
+        Me.Game_line_back.Visible = False
+        '
+        'Default_line_back
+        '
+        Me.Default_line_back.BackColor = System.Drawing.Color.Gold
+        Me.Default_line_back.Location = New System.Drawing.Point(55, 224)
+        Me.Default_line_back.Name = "Default_line_back"
+        Me.Default_line_back.Size = New System.Drawing.Size(138, 9)
+        Me.Default_line_back.TabIndex = 28
+        Me.Default_line_back.TabStop = False
+        Me.Default_line_back.Visible = False
         '
         'Form1
         '
@@ -254,14 +317,14 @@ Partial Class Form1
         Me.AutoValidate = System.Windows.Forms.AutoValidate.Disable
         Me.BackColor = System.Drawing.SystemColors.WindowFrame
         Me.CausesValidation = False
-        Me.ClientSize = New System.Drawing.Size(635, 206)
+        Me.ClientSize = New System.Drawing.Size(698, 237)
         Me.ControlBox = False
         Me.Controls.Add(Me.Code_line)
         Me.Controls.Add(Me.Music_line)
         Me.Controls.Add(Me.Game_line)
         Me.Controls.Add(Me.Default_line)
         Me.Controls.Add(Me.Code_ico)
-        Me.Controls.Add(Me.Music_ioc)
+        Me.Controls.Add(Me.Music_ico)
         Me.Controls.Add(Me.Game_ico)
         Me.Controls.Add(Me.Default_ico)
         Me.Controls.Add(Me.Code_Button)
@@ -273,6 +336,10 @@ Partial Class Form1
         Me.Controls.Add(Me.Music_text)
         Me.Controls.Add(Me.Code_text)
         Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.Code_line_back)
+        Me.Controls.Add(Me.Music_line_back)
+        Me.Controls.Add(Me.Game_line_back)
+        Me.Controls.Add(Me.Default_line_back)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -281,13 +348,17 @@ Partial Class Form1
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "桌面空间"
         CType(Me.Code_ico, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Music_ioc, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Music_ico, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Game_ico, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Default_ico, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Default_line, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Game_line, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Music_line, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Code_line, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Code_line_back, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Music_line_back, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Game_line_back, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Default_line_back, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -302,11 +373,17 @@ Partial Class Form1
     Friend WithEvents Music_Button As RadioButton
     Friend WithEvents Default_ico As PictureBox
     Friend WithEvents Game_ico As PictureBox
-    Friend WithEvents Music_ioc As PictureBox
+    Friend WithEvents Music_ico As PictureBox
     Friend WithEvents Code_ico As PictureBox
     Friend WithEvents Code_Button As RadioButton
     Friend WithEvents Default_line As PictureBox
     Friend WithEvents Game_line As PictureBox
     Friend WithEvents Music_line As PictureBox
     Friend WithEvents Code_line As PictureBox
+    Friend WithEvents Animation_up As Timer
+    Friend WithEvents Animation_down As Timer
+    Friend WithEvents Code_line_back As PictureBox
+    Friend WithEvents Music_line_back As PictureBox
+    Friend WithEvents Game_line_back As PictureBox
+    Friend WithEvents Default_line_back As PictureBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -9,11 +9,13 @@ Public Class Form1
     Dim Now_Desktop_Path As String
     Dim Space_Name As String
     '当前选中的桌面
-    Dim Select_Desktop_Name As String
+    Dim Select_Desktop As Object
 
     ReadOnly change_Desktop_Path = "D:\Desktop-Space\"
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
         Now_Desktop_Path = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
 
         '这里要改成使用正则表达式，但是现在还不会
@@ -26,19 +28,22 @@ Public Class Form1
         '控制文字底下的绿线
         Select Case Space_Name
             Case "Default"
-                Default_line.Visible = True
                 Defau_Button.Checked = True
+
             Case "Game"
-                Game_line.Visible = True
                 Game_Button.Checked = True
+
             Case "Music"
-                Music_line.Visible = True
                 Music_Button.Checked = True
+
             Case "Code"
-                Code_line.Visible = True
                 Code_Button.Checked = True
         End Select
 
+        Default_line_back.Location = Default_line.Location
+        Game_line_back.Location = Game_line.Location
+        Music_line_back.Location = Music_line.Location
+        Code_line_back.Location = Code_line.Location
     End Sub
 
 
@@ -51,7 +56,9 @@ Public Class Form1
         Game_line.Visible = False
         Music_line.Visible = False
         Code_line.Visible = False
-        Select_Desktop_Name = "Default"
+        Select_Desktop = Default_ico
+        Animation_up.Enabled = True
+
     End Sub
 
     Private Sub Game_Button_CheckedChanged(sender As Object, e As EventArgs) Handles Game_Button.CheckedChanged
@@ -59,7 +66,8 @@ Public Class Form1
         Game_line.Visible = True
         Music_line.Visible = False
         Code_line.Visible = False
-        Select_Desktop_Name = "Game"
+        Select_Desktop = Game_ico
+        Animation_up.Enabled = True
     End Sub
 
     Private Sub Music_Button_CheckedChanged(sender As Object, e As EventArgs) Handles Music_Button.CheckedChanged
@@ -67,7 +75,8 @@ Public Class Form1
         Game_line.Visible = False
         Music_line.Visible = True
         Code_line.Visible = False
-        Select_Desktop_Name = "Music"
+        Select_Desktop = Music_ico
+        Animation_up.Enabled = True
     End Sub
 
     Private Sub Code_Button_CheckedChanged(sender As Object, e As EventArgs) Handles Code_Button.CheckedChanged
@@ -75,7 +84,8 @@ Public Class Form1
         Game_line.Visible = False
         Music_line.Visible = False
         Code_line.Visible = True
-        Select_Desktop_Name = "Code"
+        Select_Desktop = Code_ico
+        Animation_up.Enabled = True
     End Sub
 
     '如果在选中的桌面上按下Enter和Esc，做出相应的动作，设置桌面或者退出
@@ -83,7 +93,7 @@ Public Class Form1
 
         If e.KeyCode = Keys.Enter Then
 
-            Set_Desktop(Select_Desktop_Name)
+            Set_Desktop(Select_Desktop)
 
         ElseIf e.KeyCode = Keys.Escape Then
             End
@@ -93,21 +103,21 @@ Public Class Form1
 
 
     '鼠标点击交互
-    Private Sub SetDesktop_Defaulet(sender As Object, e As EventArgs) Handles Default_ico.Click， Defau_text.Click, Game_ico.Click, Game_text.Click, Music_ioc.Click, Music_text.Click, Code_ico.Click, Code_text.Click
-        Set_Desktop(Select_Desktop_Name)
-    End Sub
+    'Private Sub SetDesktop_Defaulet(sender As Object, e As EventArgs) Handles Default_ico.Click， Defau_text.Click, Game_ico.Click, Game_text.Click, Music_ico.Click, Music_text.Click, Code_ico.Click, Code_text.Click
+    '    Set_Desktop(Select_Desktop)
+    ' End Sub
 
     '通过修改注册表修改桌面路径
 
 
-    Private Sub Set_Desktop(select_Desktop_Name As String)
+    Private Sub Set_Desktop(select_Desktop As Object)
         '如果选中的桌面已经是当前桌面，则不做任何处理，退出
-        If String.Compare(Me.Now_Desktop_Path, change_Desktop_Path & select_Desktop_Name) = 0 Then
+        If String.Compare(Me.Now_Desktop_Path, change_Desktop_Path & select_Desktop.Tag) = 0 Then
             End
 
         End If
 
-        Shell("cmd.exe /c reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"" /v ""Desktop"" /d " & change_Desktop_Path & select_Desktop_Name & " /t REG_EXPAND_SZ /f")
+        Shell("cmd.exe /c reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"" /v ""Desktop"" /d " & change_Desktop_Path & select_Desktop.Tag & " /t REG_EXPAND_SZ /f")
         Shell("cmd.exe /c taskkill /f /im explorer.exe & start explorer.exe")
         End
     End Sub
@@ -117,4 +127,102 @@ Public Class Form1
 
     End Sub
 
+
+    '鼠标交互
+
+    Private Sub TurnOff_AllBackLine() '关闭所有橘色的背景线
+        Default_line_back.Visible = False
+        Game_line_back.Visible = False
+        Music_line_back.Visible = False
+        Code_line_back.Visible = False
+    End Sub
+
+    '鼠标悬停
+    Private Sub Default_MouseMove(sender As Object, e As MouseEventArgs) Handles Default_ico.MouseMove, Defau_text.MouseMove
+        TurnOff_AllBackLine()
+        Default_line_back.Visible = True
+    End Sub
+
+    Private Sub Game_MouseMove(sender As Object, e As MouseEventArgs) Handles Game_ico.MouseMove, Game_text.MouseMove
+        TurnOff_AllBackLine()
+        Game_line_back.Visible = True
+    End Sub
+
+    Private Sub Music_MouseMove(sender As Object, e As MouseEventArgs) Handles Music_ico.MouseMove, Music_text.MouseMove
+        TurnOff_AllBackLine()
+        Music_line_back.Visible = True
+    End Sub
+
+    Private Sub Code_MouseMove(sender As Object, e As MouseEventArgs) Handles Code_ico.MouseMove, Code_text.MouseMove
+        TurnOff_AllBackLine()
+        Code_line_back.Visible = True
+    End Sub
+
+
+    '检测鼠标，如果鼠标移出图标范围，则关闭所有背景线(_line_back)
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, Default_line_back.MouseMove, Game_line_back.MouseMove, Music_line_back.MouseMove, Code_line_back.MouseMove
+        TurnOff_AllBackLine()
+
+    End Sub
+
+    '鼠标按下
+
+    Private Sub Default_MouseDown(sender As Object, e As MouseEventArgs) Handles Default_ico.MouseDown, Defau_text.MouseDown
+        Defau_Button.Checked = True
+    End Sub
+
+    Private Sub Game_MouseDown(sender As Object, e As MouseEventArgs) Handles Game_ico.MouseDown, Game_text.MouseDown
+        Game_Button.Checked = True
+    End Sub
+
+    Private Sub Music_MouseDown(sender As Object, e As MouseEventArgs) Handles Music_ico.MouseDown, Music_text.MouseDown
+        Music_Button.Checked = True
+    End Sub
+
+    Private Sub Code_MouseDown(sender As Object, e As MouseEventArgs) Handles Code_ico.MouseDown, Code_text.MouseDown
+        Code_Button.Checked = True
+    End Sub
+
+    '鼠标松开
+    Private Sub Default_MouseUp(sender As Object, e As MouseEventArgs) Handles Default_ico.MouseUp, Defau_text.MouseUp
+        Set_Desktop(Select_Desktop)
+    End Sub
+
+    Private Sub Game_MouseUp(sender As Object, e As MouseEventArgs) Handles Game_ico.MouseUp, Game_text.MouseUp
+        Set_Desktop(Select_Desktop)
+    End Sub
+
+    Private Sub Music_MouseUp(sender As Object, e As MouseEventArgs) Handles Music_ico.MouseUp, Music_text.MouseUp
+        Set_Desktop(Select_Desktop)
+    End Sub
+
+    Private Sub Code_MouseUp(sender As Object, e As MouseEventArgs) Handles Code_ico.MouseUp, Code_text.MouseUp
+        Set_Desktop(Select_Desktop)
+    End Sub
+
+
+
+    '   '按键控制时的图标动画
+    '   Dim a = 5
+    '   Private Sub Anima_up(sender As Object, e As EventArgs) Handles Animation_up.Tick
+    '
+    '       Select_Desktop.Location = New Point(Select_Desktop.Location.X, Select_Desktop.Location.Y - Me.a)
+    '       Me.a -= 1
+    '       If Me.a = -6 Then
+    '           Me.a = 5
+    '
+    '           Animation_up.Enabled = False
+    '
+    '
+    '       End If
+    '   End Sub
+    '   '图标下降动画
+    '   Private Sub Anima_down(sender As Object, e As EventArgs) Handles Animation_down.Tick
+    '       Select_Desktop.Location = New Point(Select_Desktop.Location.X, Select_Desktop.Location.Y + Me.a)
+    '       Me.a -= 1
+    '       If Me.a = 0 Then
+    '           Me.a = 5
+    '           Animation_down.Enabled = False
+    '       End If
+    '   End Sub
 End Class
